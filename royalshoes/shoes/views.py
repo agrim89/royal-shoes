688e00d776ee23dc8748c5576f9dafa80b3c0770
Agrim <agrim89@gmail.com>
2018-03-28 16:52:02 +0530

    upated

--- royalshoes/shoes/views.py
+++ royalshoes/shoes/views.py
@@ -97,14 +97,14 @@ class ValidateViewSet(APIView):
             user = Registration.objects.filter(mobile=mobile, password=password)
             validate = Registration.objects.filter(mobile=mobile)
             if user and validate:
-                payload = dict(message="login successful")
-                return Response(payload, status=status.HTTP_200_OK)
+                payload = dict(message="login successful", status=status.HTTP_200_OK)
+                return Response(payload)#, status=status.HTTP_200_OK)
             elif not validate:
-                payload = dict(message="user not found")
-                return Response(payload, status=status.HTTP_404_NOT_FOUND)
+                payload = dict(message="user not found",  status=status.HTTP_404_NOT_FOUND)
+                return Response(payload)#, status=status.HTTP_404_NOT_FOUND)
         except Exception:
-            payload = dict(message="login fail")
-            return Response(payload, status=status.HTTP_400_BAD_REQUEST)
+            payload = dict(message="login fail", status=status.HTTP_400_BAD_REQUEST)
+            return Response(payload)#, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CompanyDetailViewSet(APIView):
